b30d6a5d08862d9e7464851846d012d0368e4b97
atirak <59160624@go.buu.ac.th>
2019-05-20 21:12:50 +0700

    update

--- models/Group.model.js
+++ models/Group.model.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Group = new Schema({
-    groupID: Number,
-    amount: Number
+    groupID: {type:Number},
+    courseID:{type:String},
+    student: {type:[String]},
+    amount: {type:Number},
+
 },{
     collection: 'group'
 });
