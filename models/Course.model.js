0539c14e995784a1ee29d888c17760fb1b1c8a03
atirak <59160624@go.buu.ac.th>
2019-05-21 02:30:55 +0700

    near fianl

--- models/Course.model.js
+++ models/Course.model.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Course= new Schema({
-
-    subjectID: { type: String },
     subjectName: { type: String },
     teacherName: { type: [String] }
 },{
